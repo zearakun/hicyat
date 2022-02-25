98ee5f522afdd5ecfcd7ccae43c7085261c7b6e4
zearakun <91083604+zearakun@users.noreply.github.com>
2022-02-25 21:50:37 +0900

    Update cyber.py

--- hycyat/cyber.py
+++ hycyat/cyber.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-def hicyat.at(url, sleep):
+def hicyatat(url, sleep):
   proxy = {
     'http':'socks5://127.0.0.1:9050',
     'https':'socks5://127.0.0.1:9050'
@@ -9,5 +9,5 @@ def hicyat.at(url, sleep):
   r = requests.get(url,proxies=proxy)
   sleep(sleep)
 
-def hicyat.eval(code):
+def hicyateval(code):
   eval("async def eval_expr():\n",code)
