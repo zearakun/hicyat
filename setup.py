f97ce622cf7b156dcf6c2676f030ffa717ca51d2
zearakun <91083604+zearakun@users.noreply.github.com>
2022-02-25 21:38:26 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hicyat',
+    description="""requests extension package
+A Python wrapper for the hicyat API""",
+    version='0.0.1',
+    packages=find_packages()
+    author="Za"
+)
